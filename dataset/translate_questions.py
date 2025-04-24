acecdb7cae96a5b9098cd489f0c96702c1c555a1
KuznetsovAV <kav.logrus@gmail.com>
2025-04-24 19:26:44 +0400

    improve translating

--- dataset/translate_questions.py
+++ dataset/translate_questions.py
@@ -1,17 +1,45 @@
 import pandas as pd
-from datasets import Dataset
+
+GOOGLE_TRANSLATOR = "google"
+DEEPL_TRANSLATOR = "deepl"
 
 
 def translate_with_google(df: pd.DataFrame):
     from deep_translator import GoogleTranslator
 
-    translator = GoogleTranslator(source='en', target='ru')
-    df['question_ru'] = df['question'].progress_apply(lambda x: translator.translate(x))
+    translator = GoogleTranslator(source="en", target="ru")
+    df["question_ru"] = df["question"].progress_apply(lambda x: translator.translate(x))
 
-def main():
-    df = pd.read_parquet("dataset/spider.parquet", sep=";")
-    dataset_with_schema = Dataset.from_pandas(df)
+
+def translate_with_deepl(df: pd.DataFrame):
+    import deepl
+    from secret import DEEPL_API_KEY
+
+    deepl_client = deepl.DeepLClient(DEEPL_API_KEY)
+
+    df["question_ru"] = df["question"].progress_apply(
+        lambda x: deepl_client.translate_text(x, source_lang="EN", target_lang="RU")
+    )
+
+
+def run(translator_type: str = None):
+    df = pd.read_parquet("dataset/data/spider.parquet")
+
+    if translator_type == GOOGLE_TRANSLATOR:
+        print("Перевод вопросов пользователей")
+        translate_with_google(df)
+    elif translator_type == DEEPL_TRANSLATOR:
+        print("Перевод вопросов пользователей")
+        translate_with_deepl(df)
+    else:
+        print("Не указан тип используемого переводчика")
+        return
+    
+    # Сохраняем вопросы на английском и русском языках
+    df[["question", "question_ru"]].to_csv("dataset/data/questions-ru.csv", sep=";")
+
+    df.to_parquet("dataset/data/spider.parquet")
 
 
 if __name__ == "__main__":
-    main()
+    run()
