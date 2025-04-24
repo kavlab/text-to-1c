c759f49dc698a85a550d0af931563eee041964a2
KuznetsovAV <kav.logrus@gmail.com>
2025-04-24 19:27:22 +0400

    refactoring and final dataset creating

--- dataset/prepare_dataset.py
+++ dataset/prepare_dataset.py
@@ -1,13 +1,21 @@
 import convert_queries
+import create_final_dataset
 import filter_exec_queries
 import load_dataset
 import translate_questions
 
+# Загрузка датасета Spider с HuggingFace и подготовка для дальнейшей обработки
+load_dataset.run()
 
-load_dataset.main()
+# Конвертация SQL-запросов в запросы на языке 1С
+convert_queries.run()
 
-convert_queries.main()
+# Фильтрация датасета - в результате остаются только те запросы,
+# которые были успешно выполнены в тестовой базе 1С
+filter_exec_queries.run()
 
-filter_exec_queries.main()
+# Перевод вопросов пользователей с английского языка на русский
+translate_questions.run(translate_questions.GOOGLE_TRANSLATOR)
 
-translate_questions.main()
+# Формирования финального датасета, который будет использоваться для обучения моделей
+create_final_dataset.run()
