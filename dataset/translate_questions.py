0529f3fd9af021740bd8ba64e112d143785150ba
KuznetsovAV <kav.logrus@gmail.com>
2025-04-29 18:46:35 +0400

    improve questions translating

--- dataset/translate_questions.py
+++ dataset/translate_questions.py
@@ -1,12 +1,16 @@
+import os
+
 import pandas as pd
-import tqdm.auto
 
 GOOGLE_TRANSLATOR = "google"
 DEEPL_TRANSLATOR = "deepl"
 
 
 def translate_with_google(df: pd.DataFrame):
+    import tqdm.auto
     from deep_translator import GoogleTranslator
+
+    tqdm.auto.tqdm.pandas()
 
     translator = GoogleTranslator(source="en", target="ru")
     df["question_ru"] = df["question"].progress_apply(lambda x: translator.translate(x))
@@ -14,7 +18,10 @@ def translate_with_google(df: pd.DataFrame):
 
 def translate_with_deepl(df: pd.DataFrame):
     import deepl
+    import tqdm.auto
     from secret import DEEPL_API_KEY
+
+    tqdm.auto.tqdm.pandas()
 
     deepl_client = deepl.DeepLClient(DEEPL_API_KEY)
 
@@ -24,26 +31,36 @@ def translate_with_deepl(df: pd.DataFrame):
 
 
 def run(translator_type: str = None):
+    # Загружаем основной датасет
     df = pd.read_parquet("dataset/data/spider.parquet")
 
-    tqdm.auto.tqdm.pandas()
+    # Файл для кэша переводов
+    questions_file = f"dataset/data/questions-ru-{translator_type}.csv"
 
-    if translator_type == GOOGLE_TRANSLATOR:
-        print("Перевод вопросов пользователей")
-        translate_with_google(df)
-    elif translator_type == DEEPL_TRANSLATOR:
-        print("Перевод вопросов пользователей")
-        translate_with_deepl(df)
+    # Проверяем, есть ли уже файл с переводами
+    if translator_type in (GOOGLE_TRANSLATOR, DEEPL_TRANSLATOR) and os.path.exists(
+        questions_file
+    ):
+        print(f"Загружаем готовые переводы из {questions_file}")
+        cache_df = pd.read_csv(questions_file, sep=";")
+        # Объединяем столбец question_ru из кеша по индексу
+        df = df.join(cache_df[['question_ru']], how='left')
     else:
-        print("Не указан тип используемого переводчика")
-        return
-    
-    # Сохраняем вопросы на английском и русском языках для ручной проверки
-    df[["question", "question_ru"]].to_csv(
-        f"dataset/data/questions-ru-{translator_type}.csv",
-        sep=";"
-    )
+        # Если файл не найден или не указан тип переводчика
+        if translator_type == GOOGLE_TRANSLATOR:
+            print("Перевод вопросов пользователей с помощью Google Translate")
+            translate_with_google(df)
+        elif translator_type == DEEPL_TRANSLATOR:
+            print("Перевод вопросов пользователей с помощью DeepL")
+            translate_with_deepl(df)
+        else:
+            print("Не указан тип используемого переводчика")
+            return
 
+        # Сохраняем кэш переводов для последующей загрузки
+        df[["question", "question_ru"]].to_csv(questions_file, sep=";", index=False)
+
+    # Сохраняем обновленный датасет
     df.to_parquet("dataset/data/spider.parquet")
 
 
